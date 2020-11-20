7723846ca044e2e0083cdd0fb6575f3f8d61577c
Nikolajus Karpovas <superheromatch@gmail.com>
2020-11-20 15:04:48 +0200

    Update config package

--- internal/config/consumer.go
+++ internal/config/consumer.go
@@ -1,5 +1,5 @@
 /*
-  Copyright (C) 2019 - 2020 MWSOFT
+  Copyright (C) 2019 - 2021 MWSOFT
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
@@ -15,7 +15,7 @@ package config
 
 // Consumer holds the configuration values for the Kafka consumer.
 type Consumer struct {
-	Brokers []string `env:"KAFKA_BROKERS" default:"[192.168.0.105:9092]"`
-	Topic   string   `env:"KAFKA_STORE_CHOICE_CHOICE_TOPIC" default:"store.choice.choice"`
-	GroupID string   `env:"KAFKA_CONSUMER_CHOICE_GROUP_ID" default:"consumer-choice-group"`
+	Brokers []string `env:"KAFKA_BROKERS" yaml:"brokers" default:"[192.168.0.64:9092]"`
+	Topic   string   `env:"KAFKA_STORE_CHOICE_CHOICE_TOPIC" yaml:"topic" default:"store.choice.choice"`
+	GroupID string   `env:"KAFKA_CONSUMER_CHOICE_GROUP_ID" yaml:"group_id" default:"consumer.choice.group"`
 }
